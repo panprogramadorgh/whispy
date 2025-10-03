83fa88efddd7c9e691a46f5da116bf51b9a60b69
panprogramadorgh <barreroalvaro2007@gmail.com>
2025-10-03 23:47:13 +0200

    Adding cmake files to .gitignore

--- cmake-dev-build/CMakeFiles/whisperpy.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for whisperpy.
-# This may be replaced when dependencies are built.
--- cmake-dev-build/src/backend/whisper.cpp/ggml/src/CMakeFiles/ggml-cpu.dir/ggml-cpu/hbm.cpp.o.d
+++
@@ -1,3 +0,0 @@
-src/backend/whisper.cpp/ggml/src/CMakeFiles/ggml-cpu.dir/ggml-cpu/hbm.cpp.o: \
- /app/src/backend/whisper.cpp/ggml/src/ggml-cpu/hbm.cpp \
- /usr/include/stdc-predef.h
